989cfe88093e8784e647e46bb01f282b07b46d49
AngelMsger <AngelMsger@Vip.QQ.Com>
2018-05-10 13:20:44 +0800

    Init Repo.

---
+++ README.md
@@ -0,0 +1,18 @@
+# CF-LSH-HE
+*Under Development*
+
+[中文](README_zh-CN.md)
+
+## Overview
+This is an example of a combination of **Collaborative Filtering**(CF), **Local Sensitive Hashing**(LSH), and **Homomorphic Encryption**(HE) algorithms. The project focuses on how to combine real-time recommendations with multiple data in a relatively secure environment.
+
+The core idea of the implementation lies in the fact that, based on the traditional collaborative filtering algorithm, the index is generated offline through local sensitive hashing to reduce the amount of calculation involved in the recommendation when the recommended target arrives, enhances real-time performance, and makes use of Paillier's properties of homomorphic encryption to make When the data of all parties is **DIRECTLY** exposed, multiple parties are allowed to participate in the recommended calculation to improve the accuracy of the recommendation results.
+
+### Collaborative Filtering
+The collaborative filtering algorithm uses the user's historical behavior data to find similar individuals, and predicts items that the user does not generate data through similarity, rating, and other information to obtain recommended results.
+
+### Local Sensitive Hashing
+A localized sensitive hashing algorithm is a technology that can map the objects that **may be** approximated in a high dimensional space to the same hash bucket **with a maximum probability** after dimensionality reduction. In the recommendation algorithm, this technique helps us to reduce the number of users or individuals involved in the calculation without having to traverse the entire data set.
+
+### Homomorphic Encryption
+The homomorphic encryption algorithm is aimed at a cryptographic algorithm that satisfies a certain characteristic, so that we can directly calculate the ciphertext, and the value obtained after the calculation result is decrypted is the same as the result of directly calculating the plaintext. With homomorphic encryption, we are expected to participate in computing without touching the original plaintext data. The Paillier algorithm used in this project has additive homomorphism properties. I mentioned "direct" in the foregoing because I did not delve deeper into the possibility that the malicious participant could try to reverse the original data by using a large amount of counterfeit data to continuously probe the calculation results. Therefore, the possibility of this situation cannot be ruled out. . If you understand this, welcome to contact me and discuss.
---
+++ utils.py
@@ -0,0 +1,71 @@
+from csv import DictReader
+import os
+import numpy as np
+import h5py
+
+
+def read_from_csv(ratings_csv, movies_csv, tables=4, d=8):
+    ratings = {}
+    with open(ratings_csv, 'r', newline='') as f:
+        reader = DictReader(f)
+        for row in reader:
+            ratings.setdefault(int(row['userId']), {})
+            ratings[int(row['userId'])][int(row['movieId'])] = float(row['rating'])
+
+    movies = {}
+    with open(movies_csv, 'r', newline='') as f:
+        reader = DictReader(f)
+        for row in reader:
+            movies[int(row['movieId'])] = row['title']
+
+    used_movies = set()
+    for _, user_movies in ratings.items():
+        for movie_id in user_movies:
+            used_movies.add(movie_id)
+
+    user_zip_map = {}
+    for i, user_id in enumerate(ratings):
+        user_zip_map[user_id] = i
+
+    movie_zip_map = {}
+    new_movies = []
+    for i, movie_id in enumerate(used_movies):
+        new_movies.append(movies[movie_id])
+        movie_zip_map[movie_id] = i
+
+    mat = np.zeros((len(ratings), len(used_movies)))
+    for user_id, user_movies in ratings.items():
+        for movie_id, rating in user_movies.items():
+            mat[user_zip_map[user_id], movie_zip_map[movie_id]] = rating
+
+    hashes = [np.random.uniform(-1, 1, (d,)) for _ in range(tables)]
+
+    return mat, new_movies, hashes
+
+
+def write_to_hdf5(hdf5_filename, ratings, movies, hashes, attrs) -> None:
+    with h5py.File(hdf5_filename, 'w') as f:
+        f.create_dataset('ratings', data=ratings)
+        f.create_dataset('movies', data=movies)
+        group = f.create_group('hashes')
+        for i, hashes_group in enumerate(hashes):
+            group.create_dataset('hash_table_%s' % i, data=hashes_group)
+
+        for name, attr in attrs.items():
+            f.attrs[name] = attr
+
+
+def read_with_cache(ratings_csv, movies_csv, tables=4, d=8, hdf5_filename='data.hdf5', force_recreate=False):
+    if not force_recreate and os.path.exists(hdf5_filename):
+        with h5py.File(hdf5_filename, 'r') as f:
+            if ratings_csv == f.attrs['ratings_csv'] and movies_csv == f.attrs['movies_csv']:
+                return np.array(f['ratings']), np.array(f['movies']),\
+                       [np.array(hashes_group) for hashes_group in f['hashes']]
+
+    ratings, movies, hashes = read_from_csv(ratings_csv, movies_csv, tables, d)
+
+    write_to_hdf5(hdf5_filename, ratings, movies, hashes, {
+        'ratings_csv': ratings_csv,
+        'movies_csv': movies_csv
+    })
+    return ratings, movies, hashes
